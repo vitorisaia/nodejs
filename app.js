2cf3692f6a6810725b8a7d2ee38cfda1f1c2bf3b
Vitor Isaia <vitor@wplex.com.br>
2018-06-28 14:23:11 -0300

    new views for newstuff

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express() //ou require('express')()
 
 app.set('view engine', 'ejs')
 
-app.get('/tecnologia', function(req, res){
-    res.render('secao/tecnologia')
+app.get('/', function(req, res){
+    res.render('home/index')
 })
 
-app.get('/', function(req, res){
-    res.send('<html><body>Portal de noteecias</body></html>') //send é do express; end é node puro
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia')
+})
+
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias')
 })
 
 app.listen(3000, function() {

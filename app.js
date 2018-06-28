8bf299de48b746d7984f96c34c801c59b6aecdba
Vitor Isaia <vitor@wplex.com.br>
2018-06-28 14:10:33 -0300

    ejs views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express') //fetch express
 var app = express() //ou require('express')()
 
-app.get('/', function(req, res){
-    res.send('<html><body>Portal de noteecias</body></html>') //send é do express; end é node puro
-})
+app.set('view engine', 'ejs')
 
 app.get('/tecnologia', function(req, res){
-    res.send('<html><body>Noticias de tecnologia</body></html>') //send é do express; end é node puro
+    res.render('secao/tecnologia')
+})
+
+app.get('/', function(req, res){
+    res.send('<html><body>Portal de noteecias</body></html>') //send é do express; end é node puro
 })
 
 app.listen(3000, function() {

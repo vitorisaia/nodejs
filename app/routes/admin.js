5e96e9ae496ba9076d1e1980844cc0be8c4b25d7
Vitor Isaia <vitor@wplex.com.br>
2018-07-03 10:57:16 -0300

    body-parser for forms

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,9 @@ module.exports = function(app) {
     app.get('/formulario_inclusao_noticia', function(req, res){
         res.render('admin/form_add_noticia')
     })
+
+    app.post('/noticias/salvar', function(req, res){
+        const noticias = req.body
+        res.send(noticias)
+    })
 }
--- config/server.js
+++ config/server.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var consign = require('consign')
+var bodyParser = require('body-parser')
 
 var app = express()
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
+
+//body-parser é um middleware, tem que vir antes do consign;
+//tem que botar isso pra o conteúdo dos forms virem traduzidos para JSON
+app.use(bodyParser.urlencoded({extended:true}))
 
 consign().include('app/routes')
 .then('config/dbConnection.js') //se não colocar a extensão ele acha que é pasta

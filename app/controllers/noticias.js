9189abb08aa23b3ca90dbda66d87e84a570e785d
Vitor Isaia <vitor@wplex.com.br>
2018-07-10 07:52:39 -0300

    fetch news by id

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -1,6 +1,6 @@
 module.exports.noticias = function(app,req, res) {
     var connection = app.config.dbConnection()
-    var dao = new app.app.models.NoticiasDAO(connection); //second app is the app folder
+    var dao = new app.app.models.NoticiasDAO(connection); //second app is the app folder, cus it loads from the root
 
     dao.getNoticias(function(error, result) {
         res.render('noticias/noticias', {noticias : result})})
@@ -8,8 +8,10 @@ module.exports.noticias = function(app,req, res) {
 
 module.exports.noticia =  function(app, req, res){
     var connection = app.config.dbConnection()
-    var dao = new app.app.models.NoticiasDAO(connection); //second app is the app folder
+    var dao = new app.app.models.NoticiasDAO(connection); //second app is the app folder, cus it loads from the root
 
-    dao.getNoticia(function(error, result) {
+    let id_noticia = req.query.id_noticia
+
+    dao.getNoticia(id_noticia, function(error, result) {
         res.render('noticias/noticia', {noticia : result})})
 }
